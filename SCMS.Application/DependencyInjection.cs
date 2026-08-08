df7792fa4b578c8085d8238c5e8083391a809b52
@dhanasekarraman <dhanasekar.raman@aravind.org>
2026-08-08 15:36:52 +0530

    DI for Tenant's Repositories and Services

--- SCMS.Application/DependencyInjection.cs
+++ SCMS.Application/DependencyInjection.cs
@@ -1,6 +1,8 @@
 using Microsoft.Extensions.DependencyInjection;
+using scms.Application.Interfaces.Tenant;
 using scms.Application.Services;
 using scms.Application.Services.SCMS;
+using scms.Application.Services.Tenant;
 
 namespace scms.Application;
 
@@ -8,12 +10,24 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        services.AddAutoMapper(cfg => {}, typeof(Mapper.MappingProfile).Assembly);
+        services.AddAutoMapper(cfg => {}, typeof(Mapper.MappingProfile).Assembly, typeof(Mapper.TenantMappingProfile).Assembly);
+        
+        // SCMS Services
         services.AddScoped<IModuleService, ModuleService>();
         services.AddScoped<IModulePermissionService, ModulePermissionService>();
         services.AddScoped<IPlanService, PlanService>();
         services.AddScoped<IPlanModuleService, PlanModuleService>();
         services.AddScoped<ITenantResolveService, TenantResolveService>();
+        
+        // Tenant Services
+        services.AddScoped<IDepartmentService, DepartmentService>();
+        services.AddScoped<IEmployeeService, EmployeeService>();
+        services.AddScoped<IUserService, UserService>();
+        services.AddScoped<IRoleService, RoleService>();
+        services.AddScoped<IPermissionService, PermissionService>();
+
+        // Validators
+        // FluentValidation automatically scanned in API layer usually, but we can register if needed.
         return services;
     }
 }
