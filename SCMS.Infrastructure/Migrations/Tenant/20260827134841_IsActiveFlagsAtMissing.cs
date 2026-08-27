0b2c23112981d9eaf7b39701c789aea81a154501
@dhanasekarraman <dhanasekar.raman@aravind.org>
2026-08-27 19:27:14 +0530

    Migration for IsActive (Role, TenantPermission)

---
+++ SCMS.Infrastructure/Migrations/Tenant/20260827134841_IsActiveFlagsAtMissing.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace scms.Infrastructure.Migrations.Tenant
+{
+    /// <inheritdoc />
+    public partial class IsActiveFlagsAtMissing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_active",
+                table: "roles",
+                type: "boolean",
+                nullable: false,
+                defaultValue: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "is_active",
+                table: "permissions",
+                type: "boolean",
+                nullable: false,
+                defaultValue: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_active",
+                table: "roles");
+
+            migrationBuilder.DropColumn(
+                name: "is_active",
+                table: "permissions");
+        }
+    }
+}
